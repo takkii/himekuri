71f75a1b80296064ad124c6ac31d13e4d7cb3d41
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-12-09 12:00:54 +0900

    Create.

---
+++ req/minitest.rb
@@ -0,0 +1,8 @@
+# --------------------------------------
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/runner'
+require 'himekurits'
+require 'tanraku'
+# --------------------------------------
