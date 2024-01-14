7f62adcc9c25d14aed64e4543e4f4828913c2ee6
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-01-14 11:43:02 +0900

    Update.

--- lib/zella.rb
+++ lib/zella.rb
@@ -14,7 +14,16 @@ class Zella
     month = td.month.to_i
     day = td.day.to_i
 
+    # 西暦が 4 で割り切れる年はうるう年とする
     if year % 4 == 0
+      calc = (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7 + 1
+
+    # 西暦が 100 でも割り切れる年はうるう年としない
+    elsif year % 100 == 0
+      calc = (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7
+
+    # 西暦が 400 でも割り切れる年はうるう年とする
+    elsif year % 400 == 0
       calc = (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7 + 1
     else
       calc = (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7
