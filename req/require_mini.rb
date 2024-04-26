6c1c59a88530431cf49acb53d073bf689d1e2edd
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-04-26 17:17:44 +0900

    Update.

---
+++ req/require_mini.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# --------------------------------------
+require 'date'
+require 'minitest/autorun'
+require 'himekuri/version'
+# --------------------------------------
